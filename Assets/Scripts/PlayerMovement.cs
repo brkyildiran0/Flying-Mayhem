1799e283b38a306cc94e66f64bcfc56e90f82c4b
Burak Yıldıran <omerburakyildiran@gmail.com>
2022-06-12 14:54:22 +0300

    Input Handling | Movement | Spaceship Assets
    - Tried both old & new input system of Unity.
    - Added movement and clamped the position relative to the screen. (Will add he rotation of the starship soon.)
    - Imported a low-poly spaceship and painted with custom materials

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xRange = 7f;
+    [SerializeField] float yRange = 5f;
 
     void Update()
     {
-        
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffset = yThrow * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange + 1f, yRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
