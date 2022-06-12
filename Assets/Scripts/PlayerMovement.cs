44cb9fad7445547fde7a0f0280f1ff8a59c02753
Burak Yıldıran <omerburakyildiran@gmail.com>
2022-06-12 17:47:57 +0300

    Spaceship Rotation
    - Starship can now rotate according to the movement made by the player.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,16 +7,34 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float xRange = 7f;
     [SerializeField] float yRange = 5f;
 
+    [SerializeField] float positionPitchFactor = 3f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float controlRollFactor = -12f;
+    [SerializeField] float controlYawFactor = -5f;
+
+
+    float xThrow, yThrow;
+
     void Update()
     {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+
         HandleMovement();
+        HandleRotation();
+    }
+     
+    void HandleRotation()
+    {
+        float pitch = transform.localRotation.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localRotation.x * controlYawFactor;
+        float roll = transform.localRotation.z + xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     void HandleMovement()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
-
         float xOffset = xThrow * controlSpeed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
