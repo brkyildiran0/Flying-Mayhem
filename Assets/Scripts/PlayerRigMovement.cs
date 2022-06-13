1e85060f82e1faccd6c9c16d8de7539d6cade2fc
Burak Yıldıran <omerburakyildiran@gmail.com>
2022-06-13 23:22:59 +0300

    Terrain & Timeline Complete Overhaul

---
+++ Assets/Scripts/PlayerRigMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerRigMovement : MonoBehaviour
+{
+    [SerializeField] float flightSpeed = 3f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+    }
+}
